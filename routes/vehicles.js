dafc3201cfb7e0473a254d93dedb13e1d5dd263b
Adrian Maslak <maslak.a92@gmail.com>
2025-04-07 18:24:18 +0200

    pridanie delete metody

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -34,4 +34,32 @@ router.post('/garage/:garageId', auth, async (req, res) => {
   
 });
 
+router.delete('/garage/:garageId/vehicle/:vehicleId', auth, async (req, res) => {
+  try {
+    // kontrola ci garaz existuje
+    const garage = await Garage.findById(req.params.garageId);
+    if (!garage) return res.status(404).json({ message: 'Garage not found' });
+
+    // verifikacia ci je pouzivatel admin
+    if (garage.admin.toString() !== req.user.id) {
+      return res.status(403).json({ message: 'Access denied' });
+    }
+
+    // kontrola ci vozidlo pod id existuje
+    const vehicle = await Vehicle.findById(req.params.vehicleId);
+    if (!vehicle) return res.status(404).json({ message: 'Vehicle not found' });
+
+    // kontrola ci vozidlo patri do garaze
+    if (vehicle.garage.toString() !== req.params.garageId) {
+      return res.status(400).json({ message: 'Vehicle does not belong to this garage' });
+    }
+
+    // delete vozidla
+    await vehicle.deleteOne();
+    res.json({ message: 'Vehicle deleted successfully' });
+  } catch (error) {
+    console.error('Server error:', error);
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+});
 module.exports = router;
