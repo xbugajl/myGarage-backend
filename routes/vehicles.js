ee49411e297f2feca843a60ff8d5f179a0e06934
Adrian Maslak <maslak.a92@gmail.com>
2025-04-07 18:29:15 +0200

    pridanie put metody na update vozidla

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -34,6 +34,7 @@ router.post('/garage/:garageId', auth, async (req, res) => {
   
 });
 
+
 router.delete('/garage/:garageId/vehicle/:vehicleId', auth, async (req, res) => {
   try {
     // kontrola ci garaz existuje
@@ -62,4 +63,41 @@ router.delete('/garage/:garageId/vehicle/:vehicleId', auth, async (req, res) => 
     res.status(500).json({ message: 'Server error', error: error.message });
   }
 });
+
+router.put('/garage/:garageId/vehicle/:vehicleId', auth, async (req, res) => {
+  const { brand, model, year, identification , photos } = req.body;
+  try {
+    // kontrola ci garaz existuje
+    const garage = await Garage.findById(req.params.garageId);
+    if (!garage) return res.status(404).json({ message: 'Garage not found' });
+
+    // kontrola ci je user admin
+    if (garage.admin.toString() !== req.user.id) {
+      return res.status(403).json({ message: 'Access denied' });
+    }
+
+    // kontrola ci ovzidlo je
+    const vehicle = await Vehicle.findById(req.params.vehicleId);
+    if (!vehicle) return res.status(404).json({ message: 'Vehicle not found' });
+
+    // kontrola ci vozidlo patri do garaze
+    if (vehicle.garage.toString() !== req.params.garageId) {
+      return res.status(400).json({ message: 'Vehicle does not belong to this garage' });
+    }
+    
+    // update fieldov pokial su vyplnene
+    if (brand) vehicle.brand = brand;
+    if (model) vehicle.model = model;
+    if (year) vehicle.year = year;
+    if (identification) vehicle.identification = identification;
+    if (photos) vehicle.photos = photos;
+
+    await vehicle.save();
+    res.json(vehicle);
+  } catch (error) {
+    console.error('Server error:', error);
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+});
+
 module.exports = router;
