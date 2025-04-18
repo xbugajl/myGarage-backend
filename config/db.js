c3f438bf94ecc0dfd085934831a4d61be1770e55
Adrian Maslak <maslak.a92@gmail.com>
2025-04-18 21:53:15 +0200

    db hosting

--- config/db.js
+++ config/db.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
 
+console.table({
+  DB_USER: process.env.DB_USER,
+  DB_PASS: process.env.DB_PASS ? '***' : undefined,
+  DB_NAME: process.env.DB_NAME,
+  CLUSTER_HOST: process.env.CLUSTER_HOST,
+});
+
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}` +
+  `@${process.env.CLUSTER_HOST}/${process.env.DB_NAME}` +
+  `?retryWrites=true&w=majority&appName=Cluster1`;
+
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/mygarage', { // URI na pripojenie na DB
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    await mongoose.connect(uri, { // URI na pripojenie na DB
     });
     console.log('MongoDB connected');
   } catch (error) {
--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+
+require('dotenv').config();
+
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
