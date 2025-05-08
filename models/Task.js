aa7a0d258ff5942fae5d504be63faf81f7078d01
Adrian Maslak <maslak.a92@gmail.com>
2025-05-08 11:29:19 +0200

    updated tasks model

--- models/Task.js
+++ models/Task.js
@@ -1,17 +1,35 @@
+// /models/Task.js
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-  description: { type: String, required: true },
-  dueDate: { type: Date, required: true },
-  assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User'}, // toto neviem ci je nutne, ale mozno je dobre to mat ked chcem priradit task iba pre specificku skupinu ludi
-  vehicle: { type: mongoose.Schema.Types.ObjectId, ref: 'Vehicle', required: true },
-  status: { type: String, enum: ['pending', 'completed'], default: 'pending' },
-  evidence: [String], // URLs or file paths
-  completedAt: { type: Date },
-  location: { // GPS location when completed 
-    latitude: { type: Number },
-    longitude: { type: Number },
+  name: {
+    type: String,
+    required: true       
   },
+  description: {
+    type: String,
+    required: true
+  },
+  dueDate: {
+    type: Date,
+    required: true
+  },
+  vehicle: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Vehicle',
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'completed'],
+    default: 'pending'
+  },
+  evidence: [String],      // URLs or file paths
+  completedAt: Date,
+  location: {             // GPS location when completed
+    latitude:  { type: Number },
+    longitude: { type: Number }
+  }
 });
 
 module.exports = mongoose.model('Task', taskSchema);
